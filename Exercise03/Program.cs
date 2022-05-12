1689e233a5d44e519c6b716707cb20fdda622fcb
33222WatanabeKiyoya <33222@ojs.ac.jp>
2022-05-12 15:47:46 +0900

    2-3完成

---
+++ Exercise03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(String[] args) {
+            int select = 0;
+            var sales = new SalesCounter("sales.csv");
+
+            Console.WriteLine("売上高の表示選択");
+            Console.WriteLine("1.店舗別");
+            Console.WriteLine("2.商品カテゴリー別");
+            Console.Write(">");
+
+            select = int.Parse(Console.ReadLine());
+            IDictionary<string, int> amountPerStore = null;
+            switch (select) {
+                case 1:
+                amountPerStore = sales.GetPerStoreSales();
+            break;
+
+                case 2:
+                amountPerStore = sales.GetPerCategorySales();
+                break;
+
+            }
+
+
+            foreach(var obj in amountPerStore) {
+                Console.WriteLine("{0}{1}",obj.Key, obj.Value);
+
+
+            }
+        }
+    }
+}
