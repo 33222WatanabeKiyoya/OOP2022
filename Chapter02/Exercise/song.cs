e0e92e81229d84761f2bfe5e7c54218c8d4b127e
33222WatanabeKiyoya <33222@ojs.ac.jp>
2022-05-12 13:53:44 +0900

    2-1完成

--- Chapter02/Exercise/song.cs
+++ Chapter02/Exercise/song.cs
@@ -11,7 +11,7 @@ namespace Exercise {
         public String ArtistName { get; set; }
         public int Length { get; set; }
 
-        public Song(string title,string artistname,int length) {
+        public Song(string title, string artistname, int length) {
             this.Title = title;
             this.ArtistName = artistname;
             this.Length = length;
