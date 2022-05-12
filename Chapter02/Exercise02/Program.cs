dd906bf63bb5c476b1816658eb35a716af401f5a
33222WatanabeKiyoya <33222@ojs.ac.jp>
2022-05-12 15:04:12 +0900

    2-2完成

--- Chapter02/Exercise02/Program.cs
+++ Chapter02/Exercise02/Program.cs
@@ -8,8 +8,39 @@ namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
 
+            if (args.Length >= 1 && args[0] == "-tom")
 
 
+                PrintMeterToInchList(1, 10);
+
+            else
+
+                PrintInchToMeterList(1, 10);
+           
+            
         }
+
+        private static void PrintInchToMeterList(int start,int stop) {
+            for(int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0} inch = {1:0.0000} m", inch, meter);
+            }
+        }
+
+        private static void PrintMeterToInchList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double inch = InchConverter.FromMeter(meter);
+                Console.WriteLine("{0} meter = {1:0.0000} m", meter, inch);
+            }
+        }
+
     }
 }
+
+  
+
+            
+
+
+
+
