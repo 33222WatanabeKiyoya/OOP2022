efa714eb7e5b98b238d331ef14dfcbd9287fcdb8
33222WatanabeKiyoya <33222@ojs.ac.jp>
2022-05-26 15:26:18 +0900

    5-1

---
+++ Chapter05/Exercise01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("文字列１を入力");
+            string input1 = Console.ReadLine();
+
+            Console.WriteLine("文字列２を入力");
+            string input2 = Console.ReadLine();
+
+            if (input1 == input2) {
+                Console.WriteLine("等しいです。");
+            }
+            else {
+                Console.WriteLine("等しくないです。");
+
+
+            }
+        }
+    }
+}
