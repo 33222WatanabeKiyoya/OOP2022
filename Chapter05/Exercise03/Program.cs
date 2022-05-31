216f4b63cc3f8a8aed780da79c08525b1b4c37d0
33222WatanabeKiyoya <33222@ojs.ac.jp>
2022-05-31 15:58:14 +0900

    Chapter6-1-1,2

--- Chapter05/Exercise03/Program.cs
+++ Chapter05/Exercise03/Program.cs
@@ -30,23 +30,44 @@ namespace Exercise03 {
 
         private static void Exercise3_1(string text) {
 
+            int spaces = text.Count(c => c == ' ');
+            Console.WriteLine("空白数:{0}", spaces);
+            
         }
 
         private static void Exercise3_2(string text) {
+            var str1 = "Jackdaws love my big sphinx of quartz";
+            var str2 = str1.Replace("big","small");
 
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+
+            Console.ReadKey();
         }
 
         private static void Exercise3_3(string text) {
-
+            var split = text.Split(' ').Length;
+            Console.WriteLine(split);
         }
+
 
         private static void Exercise3_4(string text) {
 
+           var s = text.Split(' ').Where(word => word.Length <= 4);
+           foreach(var item in s) {
+                Console.WriteLine(item);
+
+           } 
         }
 
         private static void Exercise3_5(string text) {
+            var array = text.Split(' ').ToArray();
+            if(array.Length > 0) 
+            {
+                var sb = new StringBuilder
 
 
+            }        
         }
     }
 }
