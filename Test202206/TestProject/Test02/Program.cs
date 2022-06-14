7130a9fd4f32c54d0ba517c60bd6a887fb692dd4
33222WatanabeKiyoya <104185381+33222WatanabeKiyoya@users.noreply.github.com>
2022-06-14 15:39:50 +0900

    提出用

--- Test202206/TestProject/Test02/Program.cs
+++ Test202206/TestProject/Test02/Program.cs
@@ -8,12 +8,12 @@ namespace Test02 {
             var numbers = new List<int> {
                     12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48
                 };
-            #region テスト用ドライバ
+             //テスト用ドライバ
             Console.WriteLine("問題１：合計値");
             Exercise01(numbers);
             Console.WriteLine("\n-----");
 
-            Console.WriteLine("問題２：偶数の最大値");
+           /* Console.WriteLine("問題２：偶数の最大値");
             Exercise02(numbers);
             Console.WriteLine("\n-----");
 
@@ -24,7 +24,7 @@ namespace Test02 {
             Console.WriteLine("問題４：10 以上 50 以下のみ");
             Exercise04(numbers);
             Console.WriteLine("\n-----");
-            #endregion
+            #endregion*/
         }
 
         //---------------------------------------
@@ -37,31 +37,38 @@ namespace Test02 {
         //　　　　出力結果【618】
         private static void Exercise01(List<int> numbers) {
 
+            
+            foreach (var n in numbers) {
 
-
+                Console.WriteLine(n);
+             
+            }        
         }
 
-        //問題２　偶数の最大値を表示
-        //　　　　出力結果【94】
-        private static void Exercise02(List<int> numbers) {
-
-
-
-        }
-        //問題３　昇順に並べて表示（遅延実行とする）
-        //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
-        private static void Exercise03(List<int> numbers) {
-
-
-
-        }
-
-        //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
-        //　　　　出力結果【12 14 20 40 35 31 17 48】
-        private static void Exercise04(List<int> numbers) {
-
-
-
-        }
     }
 }
+
+    //問題２　偶数の最大値を表示
+    //　　　　出力結果【94】
+    /*private static void Exercise02(List<int> numbers) {
+
+
+
+    }
+    //問題３　昇順に並べて表示（遅延実行とする）
+    //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
+    private static void Exercise03(List<int> numbers) {
+
+
+
+    }
+
+    //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
+    //　　　　出力結果【12 14 20 40 35 31 17 48】
+    private static void Exercise04(List<int> numbers) {
+
+
+
+   }
+    }
+}*/
