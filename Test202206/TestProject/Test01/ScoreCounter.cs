a06e67a0f9ddf77ae4ebab01203de4a0a65302fb
33222WatanabeKiyoya <104185381+33222WatanabeKiyoya@users.noreply.github.com>
2022-06-14 14:55:05 +0900

    Test01提出用

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -7,33 +7,50 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-            
-           
-
+            _score = ReadScore(filePath);
             
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
+
+            List<Student> students = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+
+              foreach (string line in lines) {
+
+                  string[] items = line.Split(',');
+                  Student student = new Student
+                  {
+                        Name = items[0],
+                        Subject = items[1],
+                        Score = int.Parse(items[2])
+                   };
+                students.Add(student);
+                }
+                return students;
 
 
-
-
-
-
-            
         }
 
-        //メソッドの概要： 
+            //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
 
+            var dict = new Dictionary<string, int>();
+
+            foreach (Student student in _score) {
+                if (dict.ContainsKey(student.Name))
+                    dict[student.Name] += student.Score;
+                else
+                    dict[student.Name] = student.Score;
+            }
+            return dict;
+        }
 
 
 
 
-            
         }
     }
-}
+
