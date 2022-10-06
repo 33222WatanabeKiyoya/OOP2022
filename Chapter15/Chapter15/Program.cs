be932d8110e5582121044d0a943171ebc38b64c1
33222WatanabeKiyoya <104185381+33222WatanabeKiyoya@users.noreply.github.com>
2022-10-06 13:36:08 +0900

    新規作成15章

--- Chapter15/Chapter15/Program.cs
+++ Chapter15/Chapter15/Program.cs
@@ -6,17 +6,71 @@ using System.Threading.Tasks;
 
 namespace Chapter15 {
     class Program {
-
         static void Main(string[] args) {
 
-            var query = Library.Books.Select(b => b.PublishedYear).Distinct().OrderBy(y => y);
-                        
+            var years = new List<int>();
+            int year;
 
-            foreach(var n in query) {
-
-                Console.WriteLine(n);
+            Console.WriteLine("出力したい西暦を入力（終了：-1）");
+            year = int.Parse(Console.ReadLine());
+            while (year != -1) {
+                years.Add(year);
+                year = int.Parse(Console.ReadLine());
             }
-              
+
+            int sort;
+
+            Console.WriteLine();
+            Console.Write("昇順：1 降順：2 ：");
+            sort = int.Parse(Console.ReadLine());
+
+            IEnumerable<Book> books;
+            if (sort == 1) {
+                books = Library.Books
+                    .Where(b => years.Contains(b.PublishedYear))
+                    .OrderBy(b => b.PublishedYear);
+            }
+            else {
+                books = Library.Books
+                    .Where(b => years.Contains(b.PublishedYear))
+                    .OrderByDescending(b => b.PublishedYear);
+            }
+
+            //var years = new int[] { 2013, 2016 };
+            foreach (var book in books) {
+                Console.WriteLine(book);
+            }
+
+            Console.WriteLine();
+
+            var selected = Library.Books
+               .OrderBy(b => b.CategoryId)
+               .ThenBy(b => b.PublishedYear)
+               .Join(Library.Categories,    //結合する2番目のシーケンス
+               book => book.CategoryId,      //対象シーケンスの結合キー
+               category => category.Id,   //2番目のシーケンスの結合キー
+               (book, category) => new
+
+               {
+                  Title = book.Title,
+                  Category = category.Name,
+                  PublishedYear = book.PublishedYear,
+                  price = book.Price,
+              }
+
+            ).ToList();
+
+            
+
+            foreach (var book in selected
+                                    .OrderByDescending(x=>x.PublishedYear)
+                                    .ThenBy(x=>x.Category))
+                                
+            {
+                Console.WriteLine($"{book.PublishedYear},{book.Title},{book.Category}{book.price}円");                                                  
+            }
+            Console.WriteLine($"金額の合計{selected.Sum(b => b.price)}円");
+
         }
     }
 }
