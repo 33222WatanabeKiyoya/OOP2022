3fa73d2f6713ff7b3ce30100a23067a45504e0c8
33222WatanabeKiyoya <104185381+33222WatanabeKiyoya@users.noreply.github.com>
2022-11-10 09:21:45 +0900

    1110

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,15 +14,91 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ColorChecker {
+namespace CollarChecker {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+
+        List<MyColor> stockMyColor = new List<MyColor>();
+        MyColor myColor = new MyColor();
+
         public MainWindow() {
             InitializeComponent();
+
+            DataContext = GetColorList();
+
+        }
+
+        /// <summary>
+        /// すべての色を取得するメソッド
+        /// </summary>
+        /// <returns></returns>
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            setColor();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            setColor(); //起動時に初期状態の設定値（R:0 G:0 B:0）から色を設定
+        }
+
+        //テキストボックスの値を元に色を設定
+        private void setColor() {
+            var r = byte.Parse(rValue.Text);
+            var g = byte.Parse(gValue.Text);
+            var b = byte.Parse(bValue.Text);
+            colorArea.Background = new SolidColorBrush(Color.FromRgb(r, g, b));
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var mycolor = (MyColor)((ComboBox)sender).SelectedItem;
+            var color = mycolor.Color;
+            var name = mycolor.Name;
+
+            
+
+            rSlider.Value = mycolor.Color.R;
+            gSlider.Value = mycolor.Color.G;
+            bSlider.Value = mycolor.Color.B;
+        }
+
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
+            stockList.Items.Add($"R:{((byte)rSlider.Value)}  G:{((byte)gSlider.Value)}  B:{((byte)bSlider.Value)}");
+
+            MyColor stColor = new MyColor();
+            var r = byte.Parse(rValue.Text);
+            var g = byte.Parse(gValue.Text);
+            var b = byte.Parse(bValue.Text);
+
+            stColor.Color = Color.FromRgb(r, g, b);
+
+            var colorName = ((IEnumerable<MyColor>)DataContext)
+                               .Where(c => c.Color.R == stColor.Color.R &&
+                                           c.Color.G == stColor.Color.G &&
+                                           c.Color.B == stColor.Color.B).FirstOrDefault();
+
+            stockList.Items.Insert(0, colorName?.Name ?? "R:" + rValue.Text + "G:" + gValue.Text + "B:" + bValue.Text);
+                
         }
 
         
+       
+
+
+
+    }
+
+    /// <summary>
+    /// 色と色名を保持するクラス
+    /// </summary>
+    public class MyColor {
+        public Color Color { get; set; }
+        public string Name { get; set; }
     }
 }
